1e79c1c59d3c5b1f908601ac1e276d607fb3ded8
xor2003 <>
2026-06-30 22:32:29 +0200

    tests: add copyJoystickData stub to shared misc test utility stubs

--- tests/utility_stubs.cpp
+++ tests/utility_stubs.cpp
@@ -1,2 +1,9 @@
+#include "inttype.h"
+#include "pointers.h"
+#include "input.h"
+#include "r3d.h"
+#include "slot.h"
+
 void pollJoystick(void) {}
 void gfx_setPageN(unsigned short pageNum) { (void)pageNum; }
+void copyJoystickData(uint8 FAR *ptr) { (void)ptr; }
